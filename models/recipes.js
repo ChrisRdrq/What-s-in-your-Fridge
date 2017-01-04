1e306b07cd8270611ec2265725ec799ef93fd9bb
Jacob Nunn <jacobnunn92@gmail.com>
2017-01-04 14:06:17 -0500

    tweaked recipes model

---
+++ models/recipes.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var recipeSchema = new mongoose.Schema({
+    recipe = {
+    title: string,
+    time: string,
+    ingredients : [
+       {quantity: number, unit: string, name: string},
+    ]
+}
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
