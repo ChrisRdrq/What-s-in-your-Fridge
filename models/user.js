0a570494b535cb2d9ae9b3c767b60453f1d58bd0
Jacob Nunn <jacobnunn92@gmail.com>
2017-01-05 15:40:42 -0500

    created passport authentication for login and signup

--- models/user.js
+++ models/user.js
@@ -1,12 +1,28 @@
 var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
 
 var userSchema = new mongoose.Schema({
+    local:{
     email: { type: String, required: true },
     password: { type: String, required: true },
+    },
     firstName: String,
     lastName: String,
     ingredients: Array
 });
 
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
+
+
 
