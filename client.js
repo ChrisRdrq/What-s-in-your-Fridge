d94d692e16d79a97355480b28e6d9ad3e5c98883
Katie Wood <woodkm11@gmail.com>
2017-01-09 14:35:37 -0500

    Practicing Angular

--- client.js
+++ client.js
@@ -1,2 +1,15 @@
 const app = angular.module('fridgeApp', ['ui.router']);
-app.config(function($stateProvider))
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/templates/home.html',
+            controller: 'homeCtrl',
+            controllerAs: '$ctrl'
+        });
+    $urlRouterProvider.otherwise('/');
+});
+
+app.controller('homeCtrl', function() {
+
+})
