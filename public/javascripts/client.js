d9ef7cdb0309bb2eefa9cab932754908a2ea23e2
Katie Wood <woodkm11@gmail.com>
2017-01-10 11:04:57 -0500

    Created new ingredient controller and service.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,15 +2,38 @@ const app = angular.module('fridgeApp', ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
             url: '/',
-            // templateUrl: './templates/home.html',
-            template: `<h1>Hello</h1>`,
+            templateUrl: './templates/home.html',
             controller: 'homeCtrl',
             controllerAs: '$ctrl'
         });
     $urlRouterProvider.otherwise('/');
 });
 
-app.controller('homeCtrl', function() {
-  this.title = 'Hello from angular';
-  console.log('hello from angular');
-})
+// services
+
+app.service('ingredientsService', function($http) {
+  console.log('ingredientsService is alive!');
+  // this.getIngredients = function() {
+  //   return $http.get('/fridge');
+  // };
+  this.addIngredient = function(ingredient) {
+    return $http.post('/ingredients/add', ingredient);
+  };
+});
+
+// controllers
+
+app.controller('homeCtrl', function($state, ingredientsService) {
+  this.ingredient = {
+    ingredient: ''
+  };
+  this.submit = function() {
+    ingredientsService.addIngredient(this.ingredient)
+        .then( (response) => {
+            $state.go('ingredients');
+        })
+        .catch(function(err) {
+            alert('ERROR: ' + err);
+        });
+  };
+});
