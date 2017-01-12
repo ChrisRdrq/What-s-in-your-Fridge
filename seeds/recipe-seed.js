4e5ff7979f258cca2a9b286ce3de480bd472203a
Katie Wood <woodkm11@gmail.com>
2017-01-12 10:30:55 -0500

    Corrected fridge logo link.

--- seeds/recipe-seed.js
+++ seeds/recipe-seed.js
@@ -18,61 +18,54 @@ function handleError(err) {
   return err;
 }
 
-<<<<<<< HEAD
-console.log('removing old recipies...');
-Recipe.remove({})
-.then(function() {
-  console.log('creating some new recipies...');
-=======
-console.log('removing old recipes...');
-Recipe.remove({})
-.then(function() {
-  console.log('creating some new recipes...');
->>>>>>> f857c70032afe3189ea2206c19970adc2694a439
-  const sweetSticky  = new Recipe({
-    	title: "Sweet, Sticky and Spicy Chicken",
-    	time: "22 minutes",
-    	difficulty: "Easy",
-    	ingredients : [
-       		{quantity: 2, unit: "lbs", name: "chicken breast"},
-       		{quantity: .25, unit: "cup", name: "soy sauce"},
-       		{quantity: 2, unit: "tbs", name: "honey"},
-       		{quantity: 2, unit: "tbs", name: "hot sauce"}
-    		]
-  		});
-  const parmesanCrusted = new Recipe({
-    	title: "Easy Parmesan Crusted Chicken",
-    	time: "30 minutes",
-    	difficulty: "Easy",
-    	ingredients : [
-	       {quantity: 20, unit: "ozs", name: "chicken breast"},
-	       {quantity: 4, unit: "tsp", name: "italian seasoned bread crumbs"},
-	       {quantity: .25, unit: "cup", name: "parmesan"},
-	       {quantity: .5, unit: "cup", name: "mayonaisse"}
-    		]
-  		});
-  const mayoChicken = new Recipe({
-	    title: "Mayo Chicken",
-	    time: "85 minutes",
-	    difficulty: "Easy",
-	    ingredients : [
-	       {quantity: 2, unit: "lbs", name: "chicken breast"},
-	       {quantity: 1, unit: "cup", name: "mayonaisse"},
-	       {quantity: .25, unit: "cup", name: "parmesan"},
-	       {quantity: 1, unit: "tsp", name: "salt"}
-	    	]
-	  	});
-  return Recipe.create([sweetSticky, parmesanCrusted, mayoChicken]);
-})
-.then(function(savedRecipes) {
-  console.log('Just saved', savedRecipes.length, 'recipes.');
-  return Recipe.find({});
-})
-.then(function(allRecipes) {
-  console.log('Printing all recipes:');
-  allRecipes.forEach(function(recipe) {
-    console.log(recipe);
-  });
-  quit();
-})
-.catch(handleError);
+// console.log('removing old recipes...');
+// Recipe.remove({})
+// .then(function() {
+//   console.log('creating some new recipes...');
+//   const sweetSticky  = new Recipe({
+//     	title: "Sweet, Sticky and Spicy Chicken",
+//     	time: "22 minutes",
+//     	difficulty: "Easy",
+//     	ingredients : [
+//        		{quantity: 2, unit: "lbs", name: "chicken breast"},
+//        		{quantity: .25, unit: "cup", name: "soy sauce"},
+//        		{quantity: 2, unit: "tbs", name: "honey"},
+//        		{quantity: 2, unit: "tbs", name: "hot sauce"}
+//     		]
+//   		});
+//   const parmesanCrusted = new Recipe({
+//     	title: "Easy Parmesan Crusted Chicken",
+//     	time: "30 minutes",
+//     	difficulty: "Easy",
+//     	ingredients : [
+// 	       {quantity: 20, unit: "ozs", name: "chicken breast"},
+// 	       {quantity: 4, unit: "tsp", name: "italian seasoned bread crumbs"},
+// 	       {quantity: .25, unit: "cup", name: "parmesan"},
+// 	       {quantity: .5, unit: "cup", name: "mayonaisse"}
+//     		]
+//   		});
+//   const mayoChicken = new Recipe({
+// 	    title: "Mayo Chicken",
+// 	    time: "85 minutes",
+// 	    difficulty: "Easy",
+// 	    ingredients : [
+// 	       {quantity: 2, unit: "lbs", name: "chicken breast"},
+// 	       {quantity: 1, unit: "cup", name: "mayonaisse"},
+// 	       {quantity: .25, unit: "cup", name: "parmesan"},
+// 	       {quantity: 1, unit: "tsp", name: "salt"}
+// 	    	]
+// 	  	});
+//   return Recipe.create([sweetSticky, parmesanCrusted, mayoChicken]);
+// })
+// .then(function(savedRecipes) {
+//   console.log('Just saved', savedRecipes.length, 'recipes.');
+//   return Recipe.find({});
+// })
+// .then(function(allRecipes) {
+//   console.log('Printing all recipes:');
+//   allRecipes.forEach(function(recipe) {
+//     console.log(recipe);
+//   });
+//   quit();
+// })
+// .catch(handleError);
--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -1,78 +1,72 @@
 
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-const User = require('../models/user');
-
-//connect to database
-mongoose.connect('mongodb://kathrynwood:Clover24**@ds161018.mlab.com:61018/whatsinyourfridge');
-
-// our script will not exit until we have disconnected from the db.
-function quit() {
-  mongoose.disconnect();
-  console.log('\nQuitting!');
-}
-
-// a simple error handler
-function handleError(err) {
-  console.error('ERROR:', err);
-  quit();
-  return err;
-}
-
-console.log('removing old users...');
-User.remove({})
-.then(function() {
-<<<<<<< HEAD
-  console.log('creating some new user...');
-  const chris   = new User({
-    local: {
-      email: "chris@test.com",
-      password: "cccc"
-    },
-=======
-  console.log('creating some new users...');
-  const chris   = new User({ email: "chris@test.com",
-  password: "cccc",
->>>>>>> f857c70032afe3189ea2206c19970adc2694a439
-  firstName: "chris",
-  lastName:"roderique",
-  ingredients: ["shrimp", "broccoli", "honey"] });
-
-  const josh = new User({
-    local: {
-      email: "josh@test.com",
-      password:"jjjj"
-    } ,
-  firstName:"josh",
-  lastName:"owen",
-  ingredients: ["chicken breast", "spinach", "eggs"] });
-
-  const katie = new User({
-    local: {
-      email: "katie@test.com",
-      password:"kkkk"
-    },
-  firstName:"katie",
-  lastName:"wood",
-  ingredients: [
-    "mayonnaise","pickles","ground beef"] });
-
-  return User.create([chris,josh,katie]);
-})
-.then(function(savedUser) {
-  console.log('Just saved', savedUser.length, 'users.');
-  return User.find({});
-})
-.then(function(allUser) {
-  console.log('Three users:');
-  allUser.forEach(function(user) {
-    console.log(user);
-  });
-  quit();
-})
-.catch(handleError);
-
-
+// const mongoose = require('mongoose');
+// mongoose.Promise = require('bluebird');
+// const User = require('../models/user');
+//
+// //connect to database
+// mongoose.connect('mongodb://kathrynwood:Clover24**@ds161018.mlab.com:61018/whatsinyourfridge');
+//
+// // our script will not exit until we have disconnected from the db.
+// function quit() {
+//   mongoose.disconnect();
+//   console.log('\nQuitting!');
+// }
+//
+// // a simple error handler
+// function handleError(err) {
+//   console.error('ERROR:', err);
+//   quit();
+//   return err;
+// }
+//
+// console.log('removing old users...');
+// User.remove({})
+// .then(function() {
+//   console.log('creating some new user...');
+//   const chris   = new User({
+//     local: {
+//       email: "chris@test.com",
+//       password: "cccc"
+//     },
+//   firstName: "chris",
+//   lastName:"roderique",
+//   ingredients: ["shrimp", "broccoli", "honey"] });
+//
+//   const josh = new User({
+//     local: {
+//       email: "josh@test.com",
+//       password:"jjjj"
+//     } ,
+//   firstName:"josh",
+//   lastName:"owen",
+//   ingredients: ["chicken breast", "spinach", "eggs"] });
+//
+//   const katie = new User({
+//     local: {
+//       email: "katie@test.com",
+//       password:"kkkk"
+//     },
+//   firstName:"katie",
+//   lastName:"wood",
+//   ingredients: [
+//     "mayonnaise","pickles","ground beef"] });
+//
+//   return User.create([chris,josh,katie]);
+// })
+// .then(function(savedUser) {
+//   console.log('Just saved', savedUser.length, 'users.');
+//   return User.find({});
+// })
+// .then(function(allUser) {
+//   console.log('Three users:');
+//   allUser.forEach(function(user) {
+//     console.log(user);
+//   });
+//   quit();
+// })
+// .catch(handleError);
+//
+//
 
 
 // var userData = [
